ba12195a14f6174c50f60045ae7ffead12f968a7
kkasior <karol.kasior@gmail.com>
2020-08-29 14:31:59 +0200

    implemented podcasts test

--- src/test/java/FirstTests.java
+++ src/test/java/FirstTests.java
@@ -5,6 +5,13 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
 
 
 public class FirstTests {
@@ -22,12 +29,36 @@ public class FirstTests {
         driver = new ChromeDriver(); //przypisanie do pustej przeglądarki, przeglądarki chrome do wykonywania testów
         driver.manage().window().maximize();
         driver.get("https://dev.to");
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        //przed każdym kolejnym findElement/s poczekaj 10 sekund, zanim wywalisz error, co sekundę sprawdzaj czy element jest już dostępny
     }
 
     @Test
-    public void firstTest(){
+    public void goToPodcastsAndSelectThirdOneAndPlayIt(){
         WebElement podcastBtn = driver.findElement(By.xpath("//a[@href='/pod']"));
         highlightElement(driver,podcastBtn);
+        podcastBtn.click();
+        String podcastUrl = driver.getCurrentUrl();
+        //przejscie na inna strone z podcastami
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.urlToBe("https://dev.to/pod"));
+        //sprawdzaj co sekundę, czy url ma wartość https://dev/to/pod, jeśli w ciągu 10 sekund nie ma takiej wartości - wywal błąd
+        List<WebElement> podcasts = driver.findElements(By.tagName("h3"));
+//        for (WebElement podcast: podcasts) {
+//            highlightElement(driver,podcast);
+//        }
+        WebElement thirdPodcast = podcasts.get(2);
+        thirdPodcast.click();
+        WebElement recordBtn = driver.findElement(By.className("record-wrapper"));
+        recordBtn.click();
+
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.className("status-message"))));
+
+        String recordBtnClassAttribute = recordBtn.getAttribute("class");
+
+        boolean isPlaying = recordBtnClassAttribute.contains("playing");
+
+        assertTrue("podcast wasn't played",isPlaying);
     }
     @Test
     public void secondTest(){
